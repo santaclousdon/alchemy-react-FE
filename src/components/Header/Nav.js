f13767cdf34a7587524a64eed3516b6852bf34dc
santaclousdon <santaclous112@gmail.com>
2024-05-09 19:35:14 -0700

    delete: unnecessary code

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar({ activeState }) {
--- src/pages/Staking.js
+++ src/pages/Staking.js
@@ -1,7 +1,5 @@
 import Header from "../components/Header/Header";
 import "../assets/css/staking.css";
-// import { Carousel } from "react-responsive-carousel";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
